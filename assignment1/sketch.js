30209311e6c27199f1a808ca061bea5c10e4e1ab
Arva Presswala <arvapresswala29@gmail.com>
2016-10-17 12:33:40 -0400

    first assignment

---
+++ assignment1/sketch.js
@@ -0,0 +1,126 @@
+var counter = 0;
+
+function setup() {
+    createCanvas(windowWidth, windowHeight);
+}
+
+
+function draw() {
+    if (counter == 0) {
+        fill(0, 0, random(150));
+        ellipse(divide(width, 2), divide(height, 2), addition(mouseX, 10), addition(mouseY, 10));
+    }
+    if (counter == 1) {
+        fill(0, random(215), 0);
+        background(255);
+        rect(mouseX, mouseY, subtraction(mouseX, 10), subtraction(mouseY, 10));
+    }
+
+    if (counter == 2) {
+        background(70);
+        for (var i = 0; i < multiply(mouseX, 2); i = i + 50) {
+            for (var j = 0; j < multiply(mouseY, 2); j = j + 70) {
+                fill(random(150), 0, 100);
+                ellipse(i, j, 50, 50);
+                i = i + 1;
+                j = j + 1;
+            }
+        }
+    }
+    if (counter == 3) {
+        background(0);
+        stroke(0, 0, random(200));
+        strokeWeight(10);
+        line(maximum(random(0, width), mouseX), random(0, height), maximum(random(width), mouseX), random(0, height));
+        stroke(0, random(200), 0);
+        line(minimum(random(0, height), mouseY), random(0, width), minimum(random(height), mouseY), random(0, width));
+
+
+    }
+    if (counter == 4) {
+        noStroke();
+        background(255);
+        fill(0, 222, 0);
+        for (var i = 0; i < width; i = i + 10) {
+            arr = [random(50, width - 50)];
+            harr = [random(50, height - 50)];
+            ellipse(reverseArray(arr), reverseArray(harr), 50, 50)
+        }
+    }
+}
+/** This function takes in two float values and returns the sum of those two values
+ *
+ *  @param {number} x   This is the first float of the sum
+ *  @param {number} y  This is the second float of the sum
+ *  @return {number}        The sum of first and second as a float value
+ */
+function addition(x, y) {
+    return x + y;
+}
+/** This function takes in two float values and returns the difference of those two values
+ *
+ *  @param {number} x   This is the first float of the sum
+ *  @param {number} y  This is the second float of the sum
+ *  @return {number}        The difference of first and second as a float value
+ */
+function subtraction(x, y) {
+    return x - y;
+}
+/** This function takes in two float values and multiplies the two values
+ *
+ *  @param {number} x   This is the first float of the sum
+ *  @param {number} y  This is the second float of the sum
+ *  @return {number}        The multiplies answer of first and second as a float value
+ */
+function multiply(x, y) {
+    return x * y;
+}
+/** This function takes in two float values and divides those two values
+ *
+ *  @param {number} x   This is the first float of the sum
+ *  @param {number} y  This is the second float of the sum
+ *  @return {number}        The divided answer of first and second as a float value
+ */
+function divide(x, y) {
+    return x / y;
+}
+/** This function takes in two float values and returns the larger of those two values
+ *
+ *  @param {number} first   This is the first float of the sum
+ *  @param {number} second  This is the second float of the sum
+ *  @return {number}        The larger of first and second as a float value
+ */
+function maximum(x, y) {
+    if (x > y) {
+        return x;
+    }
+}
+/** This function takes in two float values and returns the smaller two values
+ *
+ *  @param {number} first   This is the first float of the sum
+ *  @param {number} second  This is the second float of the sum
+ *  @return {number}        The smaller first and second as a float value
+ */
+function minimum(x, y) {
+    if (x > y) {
+        return y;
+    }
+}
+
+// this function take all the value in the array and flips them
+
+
+function reverseArray(x) {
+    newArray = [];
+    for (var i = x.length - 1; i >= 0; i--) {
+        newArray.push(x[i]);
+    }
+    return newArray;
+}
+
+function mousePressed() {
+    counter = counter + 1;
+    if (counter > 4) {
+        counter = 0;
+    }
+}
