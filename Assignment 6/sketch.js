3f52de4bf5a5a67f5ff9a72f5b6c4672867a66f4
Arva Presswala <arvapresswala29@gmail.com>
2016-11-28 13:46:45 -0500

    Still have to add italics

---
+++ Assignment 6/sketch.js
@@ -0,0 +1,51 @@
+var txt;
+var fulltxt;
+var double, triple, four;
+var ital;
+var stop, exc, quest;
+var results, output;
+var done;
+
+function preload() {
+    done = false;
+    fulltxt = loadStrings('rhesus.txt', doneLoading);
+
+}
+
+function doneLoading() {
+    txt = fulltxt.join("\n");
+    done = true;
+}
+
+function setup() {
+
+}
+
+function draw() {
+    if (done) {
+        var par = createP('');
+        double = /\b\d\d\b/g;
+        triple = /\b\d\d\d\b/g;
+        four = /\b\d\d\d\d\b/g;
+        ital = /\b_/g;
+        stop = /[.]/g;
+        exc = /[!]/g;
+        quest = /[?]/g;
+        var resultsd = txt.match(double);
+        var resultst = txt.match(triple);
+        var resultsf = txt.match(four);
+        var resultss = txt.match(stop);
+        var resultse = txt.match(exc);
+        var resultsq = txt.match(quest);
+        var resultsi = txt.match(ital);
+        createP("2 digit:" + "  " + resultsd.length + '<br/>');
+        createP("3 digit:" + "  " + resultst.length + '<br/>');
+        createP("4 digit:" + "  " + resultsf.length + '<br/>');
+        createP("period" + "  " + resultss.length + '<br/>');
+        createP("exclamation" + "  " + resultse.length + '<br/>');
+        createP("question" + "  " + resultsq.length + '<br/>');
+        createP("italics" + "  " + resultsi.length + '<br/>');
+
+        noLoop();
+    }
+}
